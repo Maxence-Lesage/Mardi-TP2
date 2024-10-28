32a3cd2be57f8cab2521101f98925a705cdeaafd
Maxence-Lesage <maxencelesage4@gmail.com>
2024-10-28 20:20:39 +0100

    styling

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ import Draw from "./js/draw.js";
 
 const paint_canva = document.querySelector("#paint_canva");
 new Draw(paint_canva, "black", pencil_weight_slider.value);
+
+
+paint_canva.width = window.innerWidth;
+paint_canva.height = window.innerHeight * 0.8;
+window.addEventListener('resize', () => {
+  paint_canva.width = window.innerWidth;
+  paint_canva.height = window.innerHeight * 0.8;
+});
